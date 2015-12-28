b71c657a2afb82a1fa1ac3877e69909a7b921c4b
Richard Hallett <richard@richardhallett.com>
2015-12-28 19:45:33 +0000

    UniformBuffer support for float,float2,float3,float4

--- Fe/UniformBuffer.cs
+++ Fe/UniformBuffer.cs
@@ -20,13 +20,59 @@ namespace Fe
         }
 
         /// <summary>
-        /// Sets the specified uniform.
+        /// Sets a uniform's value to 4x4 matrix
         /// </summary>
         /// <param name="uniform">The uniform.</param>
         /// <param name="matrix">The matrix.</param>
         public void Set(Uniform uniform, Nml.Matrix4x4 matrix)
         {
             this._uniforms[uniform] = matrix;
+        }
+
+        /// <summary>
+        /// Sets a uniform's value to a float.
+        /// </summary>
+        /// <param name="uniform">The uniform.</param>
+        /// <param name="value">The value.</param>
+        public void Set(Uniform uniform, float value)
+        {
+            this._uniforms[uniform] = value;
+        }
+    
+        /// <summary>
+        /// Sets a uniform's value to a float2 (x,y).
+        /// </summary>
+        /// <param name="uniform">The uniform.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        public void Set(Uniform uniform, float x, float y)
+        {
+            this._uniforms[uniform] = new float[] { x, y };
+        }
+
+        /// <summary>
+        /// Sets a uniform's value to a float3 (x,y,z)
+        /// </summary>
+        /// <param name="uniform">The uniform.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="z">The z.</param>
+        public void Set(Uniform uniform, float x, float y, float z)
+        {
+            this._uniforms[uniform] = new float[] { x, y, z};
+        }
+
+        /// <summary>
+        /// Sets a uniform's value to a float4 (x,y,z,w)
+        /// </summary>
+        /// <param name="uniform">The uniform.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="z">The z.</param>
+        /// <param name="w">The w.</param>
+        public void Set(Uniform uniform, float x, float y, float z, float w)
+        {
+            this._uniforms[uniform] = new float[] { x, y, z, w};
         }
 
         /// <summary>
